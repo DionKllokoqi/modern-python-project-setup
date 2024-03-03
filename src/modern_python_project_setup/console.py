56316ff1804e4133da4b4c1befa766d86620c672
Dion Kllokoqi <dion.kllokoqi@gmail.com>
2024-03-03 12:32:34 +0100

    feat: add wikipedia random reading api

--- src/modern_python_project_setup/console.py
+++ src/modern_python_project_setup/console.py
@@ -1,13 +1,26 @@
 """Console script for modern_python_project_setup.
 """
 
+import textwrap
+
 import click
+import requests
 
 from . import __version__
+
+API_URL = "https://en.wikipedia.org/api/rest_v1/page/random/summary"
 
 
 @click.command()
 @click.version_option(version=__version__)
 def main():
-    """Modern Python library"""
-    click.echo("Hello, world!")
+    """The hypermodern Python project."""
+    with requests.get(API_URL, timeout=5) as response:  # Added timeout argument
+        response.raise_for_status()
+        data = response.json()
+
+    title = data["title"]
+    extract = data["extract"]
+
+    click.secho(title, fg="green")
+    click.echo(textwrap.fill(extract))
